4693397fd50d9d7705f73ffad6cd0ff2046499e4
COLIN <colinpaicemq@gmail.com>
2026-03-05 08:55:33 +0000

    tweaks

--- source/make.sh
+++ source/make.sh
@@ -1,27 +1,24 @@
+# compile the source and create a file extract, and a
+# load module in the specified load library
 name=extract
-rm CEE*
-rm $name
-rm $name.o
-# lp64 ilp32
+loadmodule="//'COLIN.LOAD(EXTRACT)'"
+# rm CEE*
+# rm $name
+# rm $name.o
 
 export _C89_CCMODE=1
-p1="-Wc,arch(8),target(zOSV2R3),list,source,lp64,gonum,asm,float(ieee)"
 p1="-Wc,arch(8),target(zOSV2R3),list,source,ilp32,gonum,asm,float(ieee)"
 p5="                           -I.                            "
-p7="-Wc,ASM,ASMLIB(//'SYS1.MACLIB'),INFO(ALL)         "
-p7=" "
 p7="-Wc,ASM,ASMLIB(//'SYS1.MACLIB')                   "
 p8="-Wc,LIST(c.lst),SOURCE,NOWARN64,XREF,SHOWINC -Wa,LIST(133),RENT"
-
-xlc  $p1 $p5 $p7  $p8   -c $name.c -o $name.o
-rc=$?
-echo "Return code "$?
-echo "RC="rc
+# compile it
+xlc  $p1 $p5 $p7 $p8   -c $name.c -o $name.o
+# bind it to local program and load module
 l1="-Wl,LIST,MAP,XREF,AC=1 "
-/bin/xlc $name.o  -o $name   -V   $l1    1>a
-/bin/xlc $name.o  -o "//'COLIN.LOAD(EXTRACT)'"  -V $l1    1>a
+/bin/xlc $name.o  -o $name   -V   $l1    1>extract.list
 extattr +a $name
-#/$name  G   SYS1        1
-#/$name  D  "ADCD*"      1
-./$name  RDATALIB "START*"  0
-#/$name U COLIN          0
+/bin/xlc $name.o  -o $loadmodule  -V $l1    1>lm.list
+# ./$name  G   SYS1        1
+# ./$name  D  "ADCD*"      1
+# ./$name  RDATALIB "START*"  0
+# ./$name U COLIN          0
